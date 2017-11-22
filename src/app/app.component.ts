d562fedd19288614bf7ec069e8576b835e05f5f5
Kuvi <anastasia.kuvanova@gmail.com>
2017-11-22 22:59:13 +0300

    condutionally added classes + other images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { TimerService } from './timer.service';
 interface CodeMetrics {
   text: string;
   availableAttempts: number;
+  isVerified: boolean;
 }
 
 @Component({
@@ -22,22 +23,29 @@ export class AppComponent {
     private timerServise: TimerService
   ) {
     this.codesArray = new Array<CodeMetrics>();
-    this.codesArray.push( {text:'2МПЦ9Н3СЛК5', availableAttempts: 3} );
-    this.codesArray.push( {text:'МПЯ9Н3СЛ2МС4', availableAttempts: 3} );
-    this.codesArray.push( {text:'ПХ9Н3СЛ2МТ3', availableAttempts: 3} );
-    this.codesArray.push( {text:'Т9Н3СЛ2МПС2', availableAttempts: 3} );
-    this.codesArray.push( {text:'Н3СЛ2МПЦ9И1', availableAttempts: 3} );
-    this.codesArray.push( {text:'3СЛ2МПВ9НБ6', availableAttempts: 3} );
-    this.codesArray.push( {text:'СЛ2МПГ9Н3У7', availableAttempts: 3} );
-    this.codesArray.push( {text:'Л2МПЭ9Н3СР8', availableAttempts: 3} );
+    this.codesArray.push( {text:'2МПЦ9Н3СЛК5', availableAttempts: 3, isVerified: false} );
+    this.codesArray.push( {text:'МПЯ9Н3СЛ2МС4', availableAttempts: 3, isVerified: false} );
+    this.codesArray.push( {text:'ПХ9Н3СЛ2МТ3', availableAttempts: 3, isVerified: false} );
+    this.codesArray.push( {text:'Т9Н3СЛ2МПС2', availableAttempts: 3, isVerified: false} );
+    this.codesArray.push( {text:'Н3СЛ2МПЦ9И1', availableAttempts: 3, isVerified: false} );
+    this.codesArray.push( {text:'3СЛ2МПВ9НБ6', availableAttempts: 3, isVerified: false} );
+    this.codesArray.push( {text:'СЛ2МПГ9Н3У7', availableAttempts: 3, isVerified: false} );
+    this.codesArray.push( {text:'Л2МПЭ9Н3СР8', availableAttempts: 3, isVerified: false} );
   }
 
   isEnabled(i: number) {
     return this.codesArray[i-1].availableAttempts > 0;
   }
+
+  isVerified(i: number) {
+    return this.codesArray[i-1].isVerified;
+  }
+
   onTeam1Clicked(team: number) {
-    this.isToShowForm = !this.isToShowForm;
-    this.current = team-1;
+    if (!this.codesArray[team-1].isVerified) {
+      this.isToShowForm = !this.isToShowForm;
+      this.current = team-1;
+    }
   }
 
   onCodeClicked(team: string) {
@@ -47,6 +55,7 @@ export class AppComponent {
     if (count > 0 &&
         team.toUpperCase() === this.codesArray[this.current].text) {
       this.timerServise.stopTimer();
+      this.codesArray[this.current].isVerified = true;
     }
     else 
     {
